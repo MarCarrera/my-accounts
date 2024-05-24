5414048069944d7ae2fedc9df0b890a249f3877c
Mar Carrera <kmcarreraprieto@gmail.com>
2024-05-24 07:13:36 -0600

    maquetando proyecto

--- lib/utils/constans.dart
+++ lib/utils/constans.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TColor {
-  static Color get orangeColor => Color.fromRGBO(255, 18, 22, 1);
-  static Color get purpleColor => Color(0xc189b4);
-  static Color get purpleDarkColor => Color(0x75536d);
+  static Color get orangeColor => Color.fromARGB(255, 255, 112, 69);
+  static Color get purpleColor => Color.fromARGB(255, 128, 106, 253);
+  static Color get lilaColor => Color.fromARGB(255, 199, 109, 244);
+  static Color get accentColor => Color.fromARGB(255, 148, 206, 210);
+  static Color get blueColor => Color.fromARGB(255, 102, 196, 254);
+  static Color get greenColor => Color.fromARGB(255, 158, 224, 143);
+  static Color get backgroundColor => Color.fromARGB(255, 26, 28, 31);
+  static Color get cardColor => Color.fromARGB(255, 92, 102, 107);
 }
--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -8,22 +8,93 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: TColor.orangeColor,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 200, horizontal: 40),
-            child: Column(
-              children: [
-                Container(
-                  height: 300,
-                  width: 600,
-                  decoration: BoxDecoration(
-                      color: TColor.orangeColor,
-                      borderRadius: BorderRadius.circular(20)),
-                )
-              ],
+      backgroundColor: TColor.backgroundColor,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 70, left: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Cuentas',
+                    style: TextStyle(
+                        fontSize: 34,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ));
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Card(),
+                  Card(),
+                  Card(),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 0, left: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Usuarios',
+                    style: TextStyle(
+                        fontSize: 34,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            CardUser(),
+            CardUser(),
+            CardUser(),
+            CardUser(),
+            CardUser()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding CardUser() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            width: 500,
+            decoration: BoxDecoration(
+                color: TColor.purpleColor,
+                borderRadius: BorderRadius.circular(20)),
+          )
+        ],
+      ),
+    );
+  }
+
+  Padding Card() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+      child: Column(
+        children: [
+          Container(
+            height: 300,
+            width: 500,
+            decoration: BoxDecoration(
+                color: TColor.orangeColor,
+                borderRadius: BorderRadius.circular(20)),
+          )
+        ],
+      ),
+    );
   }
 }
