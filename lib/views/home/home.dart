f726c83e80abb694f4a66e033785595e5febf8e5
Mar Carrera <kmcarreraprieto@gmail.com>
2024-06-19 22:25:06 -0600

    cambios picker fecha pago

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -4,12 +4,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:local_auth/local_auth.dart';
-import '../../data/request/request.dart';
 import '../../data/models/view_model.dart';
 import '../../data/request/service.dart';
 import '../../utils/constans.dart';
 import '../../utils/buttom_nav.dart';
+import 'package:intl/intl.dart';
 import 'dart:async';
+import 'dart:ffi';
 
 import '../../utils/showConfirm.dart';
 
@@ -22,6 +23,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final LocalAuthentication auth = LocalAuthentication();
+  DateTime date = DateTime.now();
   bool _canCheckBiometrics = false;
   bool _isAuthenticating = false;
   String _authorized = 'No autenticado';
@@ -38,8 +40,10 @@ class _HomeState extends State<Home> {
   bool noDataUser = false;
   bool reload = false;
   int indexAct = 1;
-
   int index = 0;
+
+  String formattedDate = '';
+  String formattedDate2 = '';
 
   Future<void> _reloadData() async {
     setState(() {
@@ -84,10 +88,8 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     futureAccounts = homeService.cargarCuentas();
-    futureProfiles = homeService
-        .cargarPerfiles(); // Reemplaza 'indexA' con el valor adecuado
-    //ProfileService().cargarPerfiles(indexAct.toString());
-    // mostrarUsuariosPorCuenta(idAccount: '2');
+    futureProfiles = homeService.cargarPerfiles();
+    formattedDate = DateFormat('yyyy-MM-dd').format(date);
   }
 
   @override
@@ -1134,6 +1136,36 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget date_time() {
+    return Container(
+      alignment: Alignment.bottomLeft,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 2, color: const Color(0xffC5C5C5))),
+      width: 300,
+      child: TextButton(
+        onPressed: () async {
+          DateTime? newDate = await showDatePicker(
+              context: context,
+              initialDate: date,
+              firstDate: DateTime(2020),
+              lastDate: DateTime(2100));
+          // ignore: unrelated_type_equality_checks
+          if (newDate == Null) return;
+          setState(() {
+            date = newDate!;
+            formattedDate2 = DateFormat('yyyy/MM/dd').format(date);
+            print(formattedDate2);
+          });
+        },
+        child: Text(
+          'Fecha : ${date.year} / ${date.month} / ${date.day}',
+          style: GoogleFonts.fredoka(fontSize: 15, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
   void _showModalSheetBono() async {
     showCupertinoModalPopup(
       context: context,
@@ -1159,17 +1191,10 @@ class _HomeState extends State<Home> {
               child: Container(
                 child: Column(
                   children: [
-                    Text('Fecha: 05 de Julio, 2024',
-                        style: GoogleFonts.fredoka(
-                            fontSize: 24,
-                            color: Color.fromRGBO(47, 125, 121, 0.9))),
                     SizedBox(
                       height: 15,
                     ),
-                    /*Text('0.00',
-                        style: GoogleFonts.fredoka(
-                            fontSize: 80,
-                            color: Color.fromRGBO(47, 125, 121, 0.9))),*/
+                    date_time(),
                     Padding(
                       padding: const EdgeInsets.only(left: 40, right: 40),
                       child: TextFormField(
