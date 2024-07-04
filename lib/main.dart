1adaf49834161b300279f743719191582a710c87
Mar Carrera <kmcarreraprieto@gmail.com>
2024-07-03 21:39:32 -0600

    implementando notificaciones nuevo metodo 2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:acounts_control/firebase_options.dart';
 import 'package:acounts_control/widgets/button_nav.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-
 import 'src/push_providers/push_notifications.dart';
+
+//Backfround message
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await PushNotifications.initializeApp();
+  //await PushNotifications.initializeApp();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  //Suscrips
+  await FirebaseMessaging.instance.subscribeToTopic('bitala');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Nuevo mensaje recibido');
+    print('Contenido: ${message.data}');
+
+    if (message.notification != null) {
+      print('Mensaje contiene notificacion: ${message.notification?.title}');
+      print('Mensaje contiene notificacion: ${message.notification?.body}');
+    }
+  });
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp(
     indexColor: 0,
   ));
